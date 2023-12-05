95fcbf36cf6d9645fddda1497571a41531b1eb54
Paulin BULAND <buland2001@gmail.com>
2023-12-05 21:33:57 +0100

    Ajoutez des fichiers projet.

---
+++ Nombre Mystère - Solution/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+    Création du jeu "Nombre Mystère"
+
+Le but du jeu "Nombre Mystère" est de faire deviner au joueur à nombre aléatoire choisi par le jeu en un minimum de coup.
+
+Le principe de base est le suivant :
+    - Le programme choisi un nombre au hasard (exemple : entre 0 et 100)
+    - L'utilisateur Entre un nombre
+    - Le programme vérifie la réponse et offre à l'utilisateur trois réponses possible : 
+        - C'est plus !
+        - C'est moins !
+        - Bonne réponse ! Le bon nombre était X, vous avez trouvé la réponse en Y coups
+    - Une fois la réponse trouvée, le programme demande à l'utilisateur s'il veux recommencer une partie
+
+Saurez-vous relever le challenge ?
+
+*/
+
+
+using Nombre_Mystère_Solution;
+
+bool fermerLeJeu = false;
+
+while (!fermerLeJeu)
+{
+    Console.WriteLine("Tapez '1' pour lancer une partie ou bien '2' pour arrêter le jeu :");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            //Lancement d'une partie
+            Solution solutionAuJeu = new Solution();
+
+            //Tans que la partie n'est pas terminée, on continue la partie
+            while (!solutionAuJeu._partieTerminee)
+            {
+                Console.WriteLine("Entrer un nombre :");
+                string tentativeUtilisateur = Console.ReadLine();
+
+                //On essaye de convertir la valeur entrée par l'utilisateur en int
+                if (int.TryParse(tentativeUtilisateur, out int nombreChoisi))
+                {
+                    //Si la valeur entrée est valide, on essaye la valeur
+                    Console.WriteLine(solutionAuJeu.FaireUneTentative(nombreChoisi));
+                }
+                else
+                {
+                    //Si la valeur entrée n'est pas valide, alors on affiche un message d'erreur
+                    Console.WriteLine("Veuillez entrer un nombre entier");
+                }
+            }
+
+            //Une fois la partie terminée, on attend que l'utilisateur appuie sur une touche supplémentaire
+            Console.WriteLine("Appuyer sur une touche pour continuer...");
+            Console.ReadKey();
+
+            //On nettoie la console
+            Console.Clear();
+
+            break;
+        case "2":
+            //Fermeture de l'application
+            fermerLeJeu = true;
+
+            break;
+
+        default:
+            Console.WriteLine("Veuillez choisir une option proposée");
+
+            break;
+    }
+
+}
+
---
+++ Nombre Mystère - Solution/Solution.cs
@@ -0,0 +1,77 @@
+﻿namespace Nombre_Mystère_Solution
+{
+    /// <summary>
+    /// Ce fichier contient la solution au développement du jeu "Nombre Mystère"
+    /// </summary>
+    public class Solution
+    {
+        #region Propriétés
+        private readonly int _numeroMinimum;
+        private readonly int _numeroMaximum;
+        private int _numeroATrouver = -1;
+        private int _nombreDeTentative = 0;
+        public bool _partieTerminee = false;
+
+        #endregion
+
+        #region Constructeur
+        public Solution(int numeroMinimum = 0, int numeroMaximum = 100)
+        {
+            _numeroMinimum = numeroMinimum;
+            _numeroMaximum = numeroMaximum;
+
+            LancerUnePartie();
+        }
+        #endregion
+
+        #region Méthodes publiques
+        /// <summary>
+        /// Permet de faire une tentative
+        /// </summary>
+        /// <param name="numero">Numéro à tester</param>
+        /// <returns>Renvoi le message pour l'utilisateur</returns>
+        public string FaireUneTentative(int numero)
+        {
+            _nombreDeTentative++;
+
+            if (numero < _numeroATrouver)
+            {
+                return "C'est plus !";
+            }
+            else if (numero > _numeroATrouver)
+            {
+                return "C'est moins !";
+            }
+            else
+            {
+                //On termine la partie
+                _partieTerminee = true;
+                return $"Bonne réponse ! Le bon nombre était {_numeroATrouver}, vous avez trouvé la réponse en {_nombreDeTentative} coups";
+            }
+        }
+        #endregion
+
+        #region Méthodes privées
+        /// <summary>
+        /// Permet de lancer une partie
+        /// </summary>
+        private void LancerUnePartie()
+        {
+            GenererUnNombreAleatoire();
+
+            Console.Clear();
+            Console.WriteLine("-------------------\n\rUne partie viens de commencer !\n\r-------------------");
+
+        }
+
+        /// <summary>
+        /// Fonction permettant la génération d'un nombre aléatoire
+        /// </summary>
+        private void GenererUnNombreAleatoire()
+        {
+            Random random = new Random();
+            _numeroATrouver = random.Next(_numeroMinimum, _numeroMaximum);
+        }
+        #endregion
+    }
+}
